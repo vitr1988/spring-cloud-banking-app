153784b8a1fd8fe6c646c4bfacb38c713b2a96b6
v.ivanov <vivanov@vtb.ru>
2021-07-09 17:32:38 +0400

    lab5: Поддержка альтернативы HystrixCommand (на базе resilience4j)

--- AccountService/src/main/java/com/luxoft/training/spring/cloud/controller/AccountController.java
+++ AccountService/src/main/java/com/luxoft/training/spring/cloud/controller/AccountController.java
@@ -2,7 +2,11 @@ package com.luxoft.training.spring.cloud.controller;
 
 import com.luxoft.training.spring.cloud.domain.Account;
 import com.luxoft.training.spring.cloud.service.AccountService;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequiredArgsConstructor
@@ -35,5 +40,21 @@ public class AccountController {
     @GetMapping("/get/{id}")
     public Optional<Account> getAccount(@PathVariable Long id) {
         return accountService.findById(id);
+    }
+
+    @Async
+    @Bulkhead(name = "account", type = Bulkhead.Type.THREADPOOL)
+    @TimeLimiter(name = "account")
+    @CircuitBreaker(name = "account", fallbackMethod = "fallbackMethod")
+    @GetMapping("/fallback/{id}")
+    public CompletableFuture<Long> fallback(@PathVariable Long id) {
+        if (id <= 0) {
+            throw new IllegalArgumentException("Has no accounts with negative ids");
+        }
+        return CompletableFuture.completedFuture(id);
+    }
+
+    private CompletableFuture<Long> fallbackMethod(Long id, IllegalArgumentException e) {
+        return CompletableFuture.completedFuture(-1L);
     }
 }
